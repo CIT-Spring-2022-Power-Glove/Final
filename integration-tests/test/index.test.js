5755071fbffe4c8f3a8755e989e4fcad87a95220
thefloatingtree <thefloatingtree@gmail.com>
2022-06-23 15:34:21 -0600

    fix prod loadbalancer port, add dev cloudbuild trigger prod deploy, make test hit dev server

--- integration-tests/test/index.test.js
+++ integration-tests/test/index.test.js
@@ -5,7 +5,7 @@ chai.should()
 
 describe('Index', () => {
     it('Should respond with 200', async () => {
-        const response = await axios.get("https://dev.stedi.me/")
+        const response = await axios.get("http://34.145.101.133/")
         response.status.should.be.equal(200)
     })
 })
