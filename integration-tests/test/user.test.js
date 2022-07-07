0bd5bf49157fbff825faa275b7c0182bd349569a
benvredenburg <vre19001@byui.edu>
2022-07-07 11:33:26 -0600

    uploading test file

---
+++ integration-tests/test/user.test.js
@@ -0,0 +1,30 @@
+import chai from 'chai'
+import axios from 'axios'
+import { faker } from '@faker-js/faker';
+
+chai.should();
+
+describe('User', () => {
+    it('Should create a user', async () => {
+        const response = await axios.post("http://34.145.101.133/user", {
+            "userName": faker.internet.userName('test'),
+            "email": faker.internet.email('test'),
+            // "userName": 'benjamin2',
+            // "email": 'benjamin@gmail.com',
+            "password": "N7T5PpQz!",
+            "verifyPassword": "N7T5PpQz!",
+            "accountType": "Personal",
+            "phone": faker.phone.number('###-###-#####')
+        });
+        response.status.should.be.equal(200);
+    });
+
+    it('Should login the user', async () => {
+        const response = await axios.post("http://34.145.101.133/login", {
+            "userName": "benjamin@gmail.com",
+            "password": "N7T5PpQz!",
+        });
+        
+        response.status.should.be.equal(200);
+    });
+});
