ebf14869152feac7ead08b7d00ca9b439e503848
JacobTapp23 <jacobtapp12@byui.edu>
2022-07-12 11:11:58 -0600

    added delete user test

--- integration-tests/test/user.test.js
+++ integration-tests/test/user.test.js
@@ -6,15 +6,26 @@ chai.should();
 
 describe('User', () => {
     it('Should create a user', async () => {
-        const response = await axios.post("http://34.145.101.133/user", {
+        const user = {
             "userName": faker.internet.userName('test'),
             "email": faker.internet.email('test'),
             "password": "N7T5PpQz!",
             "verifyPassword": "N7T5PpQz!",
             "accountType": "Personal",
             "phone": faker.phone.number('###-###-#####')
-        });
+        };
+        const response = await axios.post("http://34.145.101.133/user", user);
         response.status.should.be.equal(200);
+
+        const login = await axios.post("http://34.145.101.133/login", {
+            "userName": user.userName,
+            "password": user.password
+        });
+        
+        const deleteResponse = await axios.delete("http://34.145.101.133/user/" + user.userName, {
+            "headers": { "suresteps.session.token": login.data }
+        });
+        deleteResponse.data.should.be.equal('deleted user');
     });
 
     it('Should login the user', async () => {
@@ -36,4 +47,5 @@ describe('User', () => {
         });
         response.status.should.be.equal(200);
     });
+
 });
